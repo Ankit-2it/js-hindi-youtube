9375cad32bf07ede65beec0e2ad9756e4ffe7ca0
Ankit <141313766+Ankit-2it@users.noreply.github.com>
2025-01-24 09:32:17 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const name = "ankit kumar"
+let age = 23
+var marks = 88.90
+division = "1st"
+
+console.log(name);
+console.log(age);
+console.log(marks);
+console.log(division);
+
+console.table([name, age, marks, division])
+
+
+console.log(typeof division)
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert( 8 + 1 )  // we are using node.js not Browser
+
+console.log( 4 + 5 ) // code readability should be high
+
+console.log("Ankit")
+
+let name = "ANkit"
+let age = 23
+let isAllowed = true
+let state;
+
+// number => 
+// bigint =>
+// string => ""    
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object 
+
+console.log(typeof "ankit")  //undefined
+console.log(typeof null) //object
