62e71e64b3a1f7b5b8c95348fc9f2abbcbc43ed5
Ankit <141313766+Ankit-2it@users.noreply.github.com>
2025-01-24 10:37:11 +0000

    operations on dataTypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = false //true //"97.9d" //"Manju"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "97sd" => NaN
@@ -22,6 +22,52 @@ let boolIsAllowed = Boolean(isAllowed)
 
 let anyNumber = 45
 let stringAnyNumber = String(anyNumber)
-console.log(stringAnyNumber);
-console.log(typeof stringAnyNumber);
+// console.log(stringAnyNumber);
+// console.log(typeof stringAnyNumber);
 
+// *******------********** Operations *******------**********
+
+let value = 17
+let negativeValue = - value
+// console.log(negativeValue);
+
+// console.log(9+9);
+// console.log(9-7);
+// console.log(9*3);
+// console.log(9**2);
+// console.log(9%3);
+// console.log(9/3);
+
+let str1 = "ankit"
+let str2 = " kumar"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log(1 + "2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log( 3 + (4 * 5) % 3);  //using braces is a healthy practise
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 3+1
+
+// console.log(num1);
+
+let countValue = 10
+console.log(countValue);
+
+++countValue
+console.log(countValue);
+
+countValue++
+console.log(countValue);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
