34784bd1c6c43f4dde1fa4b0817cfa41bd1a9788
Ankit <141313766+Ankit-2it@users.noreply.github.com>
2025-01-24 10:04:35 +0000

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = false //true //"97.9d" //"Manju"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "97sd" => NaN
+// true => 1; false => 0;
+
+let isAllowed = "yuio"  //5
+
+let boolIsAllowed = Boolean(isAllowed)
+// console.log(boolIsAllowed);
+
+// 1 => true; 0 => false
+// "" => false
+// "ankit" => true
+
+let anyNumber = 45
+let stringAnyNumber = String(anyNumber)
+console.log(stringAnyNumber);
+console.log(typeof stringAnyNumber);
+
