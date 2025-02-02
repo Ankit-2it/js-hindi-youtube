93a0ac70b658faa5617aaaef0310f82f4c44c3ad
Ankit <141313766+Ankit-2it@users.noreply.github.com>
2025-02-02 10:30:25 +0000

    math

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+const skore = 367
+// console.log(skore);
+
+const income = new Number(150)
+// console.log(income);
+
+// console.log(income.toString().length);
+// console.log(income.toFixed(2));
+
+
+const num = 343.54567
+
+// console.log(num.toPrecision(5));
+// console.log(num.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString());          //comma separation
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4));  //absolute
+// console.log(Math.round(4.567));
+// console.log(Math.ceil(3.1));
+// console.log(Math.floor(3.9));
+// console.log(Math.min(-2,4,6,8,9));
+// console.log(Math.max(2,421,6,8,9));
+
+console.log(Math.random());
+console.log((Math.random()*10));  //btwn 0 - 10
+console.log((Math.random()*10)+1);  //btwn 1 - 10 
+// output for the above three:
+// 0.5031072579847704
+// 0.17978231004036171
+// 4.948129272388057
+
+console.log((Math.random()*10+1));  //btwn 1 - 10 
