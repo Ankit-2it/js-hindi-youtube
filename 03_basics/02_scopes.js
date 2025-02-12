dad2bd242020c262dc11c56918a8be8f08223e47
Ankit <141313766+Ankit-2it@users.noreply.github.com>
2025-02-12 06:44:10 +0000

    nested scope

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -1,6 +1,7 @@
 var c = 45              // Global Variable
 var d = 25
 let a = 20
+let z = 13
 // Scope:
 {
     let a = 100         // Local Variable
@@ -8,7 +9,10 @@ let a = 20
     var c = 300
     d = 600
 
-    console.log('inner a = ',a);
+    // console.log('inner a = ',a);
+
+    // console.log("outer z = ", z);    // Global variable can be accessed inside the scope
+                                     // Local variable can't be accessed outside the scope
     
 }
 
@@ -18,4 +22,58 @@ let a = 20
 // console.log(c);     // 300 | var datatype is accessible outside the scope
 // console.log(d);     // 600
 
-console.log("outer a = ", a);
+// console.log("outer a = ", a);
+
+
+
+
+// ++++++++++++++++++++++++++++++ Nested scope ++++++++++++++++++++++++++++++++++
+
+function global(){
+    let userName = "Ankit Kumar"
+
+    function local(){
+        let localvariable = "Z"
+
+        console.log(userName);
+    }
+    local()
+
+    // console.log(localvariable);    //ReferenceError: localvariable is not defined
+    
+    
+}
+// global()
+
+// Closures are similar concept
+
+if(true){
+    let userName1 = "Prasad Ankit"
+
+    if(userName1 == "Prasad Ankit"){
+        // console.log("bingo");
+        const surname = "kumar"
+    }
+    // console.log(surname);
+    
+}
+// console.log(userName1);
+
+
+
+//++++++++++++++++++++++++ Interesting +++++++++++++++++++++++++++
+
+console.log(addOne(5));         //can access bedore declaration
+
+function addOne(num){
+    return num+1
+}
+// addOne(5)
+
+
+console.log(addTwo(5));     //ReferenceError: Cannot access 'addTwo' before initialization
+
+const addTwo = function(num){
+    return num+2
+}
+// addTwo(5)
