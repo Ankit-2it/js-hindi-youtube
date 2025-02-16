5c506eadca8ae89ed201a2b51b1b8265775f4358
Ankit <141313766+Ankit-2it@users.noreply.github.com>
2025-02-16 03:46:47 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,63 @@
+// if
+// const isUserLoggedIn = true
+// const temp = 22
+
+// if(temp === 20){
+//     console.log("less than 30");
+    
+// }else{
+//     console.log("greater than 50");    
+// }
+
+
+// comparison operator
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+// const score = 45
+// if(score<50){
+//     let grade = "pass"
+//     console.log(`grade is ${grade}`);
+// }
+// console.log(`grade is ${grade}`);
+
+
+//Short hand property
+const marks = 800
+if(marks >= 500) console.log("pass"), console.log("hogye");
+;   //without {}
+    
+// ------------------ shift + alt + down_arrow : for copy
+
+// if/else if Statement
+
+// let day=2
+// if(day == 1){
+//     console.log("Mon");
+// }
+// else if(day==2){
+//     console.log("tue");
+    
+// }else if(day == 3){
+//     console.log("wed");
+// }else{
+//     console.log("invalid");
+    
+// }
+
+
+// &&, || operators
+
+let id = true
+let password = true
+if(id && password && 3==3){
+    console.log("user loggedIN");
+    
+}
+
+let loginGoogle = true
+let loginEmail = false
+if(loginEmail || loginGoogle){
+    console.log("user loggedIN");
+    
+}
---
+++ 04_control_flow/switch.js
@@ -0,0 +1,26 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "march"
+
+switch (month) {
+    case 1:
+        console.log("Jan");
+        break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("Mar");
+        break;
+
+    default:
+        console.log("invalid input");
+        break;
+}
