5cb7d8ed63af4c271b1a504ac0658a51dbad6ba2
Ankit <141313766+Ankit-2it@users.noreply.github.com>
2025-02-17 13:01:08 +0000

    while loop/ do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// While loop
+
+// let x = 1
+// while(x <= 10){
+//     console.log(`vlaue of x is ${x}`);
+//     x = x+1   
+// }
+
+// let arr1 = ["Hulk", "Dr.Strange", "Thor"]
+// let i = 0
+// while(i < arr1.length){
+//     console.log(arr1[i]);
+//     i++
+// }
+
+
+
+// do while
+let i = 8
+do{
+    console.log(`value of i is ${i}`);
+    i--
+}while(i>0)
